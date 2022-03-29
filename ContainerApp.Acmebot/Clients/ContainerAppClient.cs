749ef6b22163045ef16c361629beb3e9afba9726
Jeff Hollan <jeffhollan@outlook.com>
2022-03-28 22:33:35 -0700

    fleshed out binding code, untested

--- ContainerApp.Acmebot/Clients/ContainerAppClient.cs
+++ ContainerApp.Acmebot/Clients/ContainerAppClient.cs
@@ -14,15 +14,12 @@ namespace ContainerApp.Acmebot
     public class ContainerAppClient
     {
         private DefaultAzureCredential _credential;
-
-        public ContainerAppClient(DefaultAzureCredential credential) => _credential = credential;
-
-        public async Task<IList<ManagedEnvironmentCertificate>> GetCertificatesAsync() => throw new NotImplementedException();
-        public async Task<ManagedEnvironmentCertificate> GetCertificateAsync(string certificateName) => throw new NotImplementedException();
-        public async Task<string> UploadCertificateAsync(CertificatePolicyItem certificatePolicy, byte[] pfxBlob, string password)
+        private HttpClient _httpClient;
+        public ContainerAppClient(DefaultAzureCredential credential)
         {
-            var token = await _credential.GetTokenAsync(new TokenRequestContext(new[] { "https://management.azure.com//.default" }));
-            var httpClient = new HttpClient
+            _credential = credential;
+            var token = _credential.GetToken(new TokenRequestContext(new[] { "https://management.azure.com//.default" }));
+            _httpClient = new HttpClient
             {
                 BaseAddress = new Uri("https://management.azure.com/"),
                 DefaultRequestHeaders =
@@ -30,7 +27,15 @@ namespace ContainerApp.Acmebot
                     Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.Token)
                 }
             };
-            var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Put, $"/subscriptions/411a9cd0-f057-4ae5-8def-cc1ea96a3933/resourceGroups/d-certtest/providers/Microsoft.App/managedEnvironments/certtest/certificates/{certificatePolicy.CertificateName}?api-version=2022-01-01-preview")
+        }
+
+        public async Task<IList<ManagedEnvironmentCertificate>> GetCertificatesAsync() => throw new NotImplementedException();
+        public async Task<ManagedEnvironmentCertificate> GetCertificateAsync(string certificateName) => throw new NotImplementedException();
+        public async Task<string> UploadCertificateAsync(CertificatePolicyItem certificatePolicy, byte[] pfxBlob, string password)
+        {
+            var acaResource = JsonSerializer.Deserialize<dynamic>(await _httpClient.GetStringAsync($"{certificatePolicy.ContainerAppId}?api-version=2022-01-01-preview"));
+
+            var response = await _httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Put, $"{(string)acaResource.properties.managedEnvironmentId}/certificates/{certificatePolicy.CertificateName}?api-version=2022-01-01-preview")
             {
                 Content = new StringContent(JsonSerializer.Serialize(new
                 {
@@ -47,8 +52,36 @@ namespace ContainerApp.Acmebot
 
         }
 
-        internal async Task<string> GetDomainVerificationIdAsync(string containerAppId) => throw new NotImplementedException();
-        internal Task ValidateDomainAsync(string containerAppId, string[] dnsNames) => throw new NotImplementedException();
-        internal Task BindDomainAsync(string containerAppId, string[] dnsNames, string certificateName) => throw new NotImplementedException();
+        internal async Task<string> GetDomainVerificationIdAsync(string containerAppId)
+        {
+            var acaResource = JsonSerializer.Deserialize<dynamic>(await _httpClient.GetStringAsync($"{containerAppId}?api-version=2022-01-01-preview"));
+
+            return (string)acaResource.properties.customDomainVerificationId;
+        }
+        internal async Task ValidateDomainAsync(string containerAppId, string[] dnsNames)
+        {
+            // TODO: support multiple domain names
+            await _httpClient.PostAsync(containerAppId + $"/listCustomHostNameAnalysis?customHostName={dnsNames[0]}&api-version=2022-01-01-preview", null);
+        }
+        internal async Task BindDomainAsync(string containerAppId, string[] dnsNames, string certificateName)
+        {
+            // TODO: support multiple domain names
+            var acaResource = JsonSerializer.Deserialize<dynamic>(await _httpClient.GetStringAsync($"{containerAppId}?api-version=2022-01-01-preview"));
+            var environmentId = (string)acaResource.properties.managedEnvironmentId;
+            acaResource.properties.configuration.customDomains = new List<dynamic>
+            {
+                new
+                {
+                    name = dnsNames[0],
+                    certificateId = $"{environmentId}/certificates/{certificateName}",
+                    bindingType = "SniEnabled"
+                }
+            };
+
+            await _httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Put, $"{containerAppId}?api-version=2022-01-01-preview")
+            {
+                Content = new StringContent(JsonSerializer.Serialize(acaResource), System.Text.Encoding.UTF8, "application/json")
+            });
+        }
     }
 }
